bd87511e85b5152412c8827e507770fe6e9e670e
PiiaMiia <orav.piia@gmail.com>
2017-05-17 17:31:45 +0300

    some components

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ export default {
     Newtodo
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/Todolist.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="todolist">
+    <ol>
+      <li>First thing to do</li>
+      <li>Second thing to do</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todolist',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .hello {
+    text-align: left;
+    margin-left: 15px;
+  }
+
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
