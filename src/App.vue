ffa94e08b0b04d2b2d45efdb56101a61be835dba
PiiaMiia <orav.piia@gmail.com>
2017-05-18 18:48:10 +0300

    almost there

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,31 @@
 <template>
-  <div id="app">
-    <hello></hello>
-    <newtodo></newtodo>
+  <div id="app" class="container">
+    <GetTodo></GetTodo>
+    <CurrentTodos></CurrentTodos>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-import Newtodo from './components/Newtodo.vue'
+  import GetTodo from './components/GetTodo.vue'
+  import CurrentTodos from './components/CurrentTodos.vue'
+  export default {
+    components: {
+      GetTodo,
+      CurrentTodos
+    }
 
-export default {
-  name: 'app',
-  components: {
-    Hello,
-    Newtodo
   }
-}
-
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  font-weight: bold;
-  background-color: rgba(42, 171, 210, 0.31);
-  width: 500px;
-  min-height: 150px;
-  position: absolute;
-  left: 40%;
-  border-radius: 5px;
-}
+  #app {
+    background-color: rgba(42, 171, 210, 0.31);
+    width: 500px;
+    min-height: 150px;
+    position: fixed;
+    left: 50%;
+    top: 25px;
+    margin-left: -250px;
+    border-radius: 5px;
+  }
 </style>
---
+++ src/components/GetTodo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="get-todo" class="container">
+    <span id="title">Vue.js todo list</span>
+    <br>
+    <input class="form-control"
+           :value="newTodo"
+           @change="getTodo"
+           placeholder="Add new task here">
+    <button class="btn btn-primary" @click="addTodo">Add Todo</button>
+  </div>
+</template>
+<script>
+  export default{
+    methods: {
+      getTodo (e) {
+        this.$store.dispatch('getTodo', e.target.value)
+      },
+      addTodo () {
+        this.$store.dispatch('addTodo')
+        this.$store.dispatch('clearTodo')
+      }
+    },
+    computed: {
+      newTodo () {
+        return this.$store.getters.newTodo
+      }
+    }
+  }
+</script>
+
+<style>
+  #get-todo {
+    width: 400px;
+    margin-left: 25px;
+    margin-top: 25px;
+  }
+  #title {
+    text-align: right;
+  }
+</style>
